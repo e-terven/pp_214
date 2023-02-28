35726795f3eb5013527a8aab8c4a516599250474
e-terven <dparentco@gmail.com>
2023-02-28 11:52:54 +0900

    Annotation and Timer

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,13 @@
+package app.model;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Primary
+@Component
+public class Dog extends Animal {
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+}
